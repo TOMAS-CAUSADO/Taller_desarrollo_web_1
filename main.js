137bbe2058f45d1da52c1434903b929dfb4c2251
TOMAS-CAUSADO <tomas.alonso0206@gmail.com>
2022-10-14 17:14:47 -0500

    Ejercicio #2

--- main.js
+++ main.js
@@ -40,7 +40,92 @@ const ejercicio01 = ()=>{//Funciona Correctamente
   }
   
   }
- 
+
+  
+  const ejercicio2 = () => { // Funcionando Correctamente
+
+    alert("opciones: digite el numero del animal a trabajar");
+  
+    let Eleccion = prompt(
+      "opciones: 1 = Elefantes. opcion 2 = Jirafa. opcion 3 = Chimpaces"
+    );
+    var lefante = 0,
+      jirafa = 0,
+      chimpances = 0;
+    var categoria1 = 0,
+      categoria2 = 0,
+      categoria3 = 0;
+  
+    if (Eleccion >= 1 && Eleccion <= 3) {
+      if (Eleccion == 1) {
+        for (var i = 0; i < 5; i++) {
+          let edad = parseFloat(prompt("Digite la edad del Elefante #" + (i + 1)))
+          if (edad < 0) {
+            alert("Edad Ingresada No Aceptada");
+          } else if (edad >= 0 && edad <= 1) {
+            categoria1 = categoria1 + 1
+            } else if (edad > 1 && edad < 3) {
+                categoria2 = categoria2 + 1
+              } else if (edad >= 3) {
+                  categoria3 = categoria3 + 1
+                }
+              }
+            }
+          
+      
+      
+            if (Eleccion == 2) {
+              for (var i = 0; i < 5; i++) {
+                let edad = parseFloat(prompt("Digite la edad de la jirafa #" + (i + 1)))
+                if (edad < 0) {
+                  alert("Edad Ingresada No Aceptada");
+                } else if (edad >= 0 && edad <= 1) {
+                  categoria1 = categoria1 + 1
+                  } else if (edad > 1 && edad < 3) {
+                      categoria2 = categoria2 + 1
+                    } else if (edad >= 3) {
+                        categoria3 = categoria3 + 1
+                      }
+                    }
+                  }
+      
+                  if (Eleccion == 3) {
+                    for (var i = 0; i < 5; i++) {
+                      let edad = parseFloat(prompt("Digite la edad del Chimpaces #" + (i + 1)))
+                      if (edad < 0) {
+                        alert("Edad Ingresada No Aceptada");
+                      } else if (edad >= 0 && edad <= 1) {
+                        categoria1 = categoria1 + 1
+                        } else if (edad > 1 && edad < 3) {
+                            categoria2 = categoria2 + 1
+                          } else if (edad >= 3) {
+                              categoria3 = categoria3 + 1
+                            }
+                          }
+                        }
+        }else{
+          alert("la opcion ingresadad no esta registrada")
+        }
+        
+      
+   
+    let porcentaje1, porcentaje2, porcentaje3;
+    console.log(categoria1);
+    porcentaje1 = (categoria1 / 5) * 100;
+    porcentaje2 = (categoria2 / 5) * 100;
+    porcentaje3 = (categoria3 / 5) * 100;
+    console.log(
+      "El procentaje de animales con la edad de 0 a 1 es de : " + porcentaje1+"%"
+    );
+    console.log(
+      "El procentaje de animales con la edad de mas de 1 y menos de 3 años es de : " +
+        porcentaje2+"%"
+    );
+    console.log(
+      "El procentaje de animales con la edad de 3 o mas años es de : " +
+        porcentaje3+"%"
+    );
+  };
  
  
   
